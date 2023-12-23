f3620e2d2c3c0063ce2ca2249ed4bf47056e1b96
Guruvignesh05 <113780449+Guruvignesh05@users.noreply.github.com>
2023-12-23 12:44:34 +0530

    Update DungeonGame2.java
    2 question

--- DungeonGame.java
+++ DungeonGame.java
@@ -4,27 +4,50 @@ public class DungeonGame {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-           System.out.println("Enter dimensions of the dungeon (R *C): ");
+        System.out.println("Enter dimensions of the dungeon (R * C): ");
         int rows = scanner.nextInt();
         int columns = scanner.nextInt();
 
-          System.out.println("Enter position of adventure : ");
+        System.out.println("Enter position of adventure : ");
         int adventureRow = scanner.nextInt();
         int adventureColumn = scanner.nextInt();
 
-          System.out.println("Enter position of gold : ");
-         int goldRow = scanner.nextInt();
-         int goldColumn = scanner.nextInt();
+        System.out.println("Enter position of monster : ");
+        int monsterRow = scanner.nextInt();
+        int monsterColumn = scanner.nextInt();
 
-         int steps = calculateMinimumSteps(rows, columns, adventureRow, adventureColumn, goldRow, goldColumn);
-         System.out.println("Minimum number of steps: " + steps);
+        System.out.println("Enter position of gold : ");
+        int goldRow = scanner.nextInt();
+        int goldColumn = scanner.nextInt();
+
+        int steps = calculateMinimumSteps(rows, columns, adventureRow, adventureColumn, goldRow, goldColumn, monsterRow, monsterColumn);
+
+        if (steps == -1) {
+            System.out.println("No possible solution");
+        } else {
+            System.out.println("Minimum number of steps: " + steps);
+        }
     }
 
-    private static int calculateMinimumSteps(int rows, int columns, int adventureRow, int adventureColumn, int goldRow, int goldColumn) {
+    private static int calculateMinimumSteps(int rows, int columns, int adventureRow, int adventureColumn, int goldRow, int goldColumn, int monsterRow, int monsterColumn) {
 
-         int rowDifference = Math.abs(adventureRow - goldRow);
-            int columnDifference = Math.abs(adventureColumn - goldColumn);
+        if (isMonsterBlocking(adventureRow, adventureColumn, monsterRow, monsterColumn)) {
+            return -1;
+        }
 
-        return rowDifference + columnDifference;
+        if (Math.abs(monsterRow - adventureRow) == 1 || Math.abs(monsterColumn - adventureColumn) == 1) {
+            return -1;
+        }
+
+        int rowDiff = Math.abs(adventureRow - goldRow);
+        int columnDiff = Math.abs(adventureColumn - goldColumn);
+        int steps = rowDiff + columnDiff;
+
+
+        return rowDiff + columnDiff;
+    }
+
+    private static boolean isMonsterBlocking(int adventureRow, int adventureColumn, int monsterRow, int monsterColumn) {
+        return adventureRow == monsterRow && adventureColumn == monsterColumn;
     }
 }
